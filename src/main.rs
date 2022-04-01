dca2b087d9a82c67293858dc2a4a495e9bef30c9
df@ <@>
2022-04-01 16:23:07 +0100

    removed uninteresting; filtering on 1 process.

--- src/main.rs
+++ src/main.rs
@@ -9,42 +9,11 @@ fn main() {
     // First we update all information of our `System` struct.
     sys.refresh_all();
 
-    // We display all disks' information:
-    println!("=> disks:");
-    for disk in sys.disks() {
-        println!("{:?}", disk);
-    }
-
-    // Network interfaces name, data received and data transmitted:
-    println!("=> networks:");
-    for (interface_name, data) in sys.networks() {
-        println!("{}: {}/{} B", interface_name, data.received(), data.transmitted());
-    }
-
-    // Components temperature:
-    println!("=> components:");
-    for component in sys.components() {
-        println!("{:?}", component);
-    }
-
-    println!("=> system:");
-    // RAM and swap information:
-    println!("total memory: {} KB", sys.total_memory());
-    println!("used memory : {} KB", sys.used_memory());
-    println!("total swap  : {} KB", sys.total_swap());
-    println!("used swap   : {} KB", sys.used_swap());
-
-    // Display system information:
-    println!("System name:             {:?}", sys.name());
-    println!("System kernel version:   {:?}", sys.kernel_version());
-    println!("System OS version:       {:?}", sys.os_version());
-    println!("System host name:        {:?}", sys.host_name());
-
-    // Number of processors:
-    println!("NB processors: {}", sys.processors().len());
-
     // Display processes ID, name na disk usage:
     for (pid, process) in sys.processes() {
+        if !process.name().to_lowercase().contains("firefox") {
+            continue;
+        }
         println!("[{}] {} {:?}", pid, process.name(), process.disk_usage());
     }
 }
