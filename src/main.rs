6da1c8d5184c886bf756eacfa5ac1e204ab77e43
df@ <@>
2022-04-03 00:49:48 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
     };
     let delay_secs = match std::env::args().nth(2) {
         Some(secs) => {
-            match secs.parse::<i32>() {
+            match secs.parse::<u64>() {
                 Ok(num) => num,
                 _ => {
                     println!("Could not get kill delat seconfd: \"{}\"", secs);
@@ -29,7 +29,7 @@ fn main() {
     println!("--> kill time: {}", delay_secs);
     let mut sys = System::new_all();
     let check_delay = Duration::new(2, 0);
-    let kill_delay = Duration::new(30, 0);
+    let kill_delay = Duration::new(delay_secs, 0);
     let mut last_running = Instant::now();
 
     loop {
